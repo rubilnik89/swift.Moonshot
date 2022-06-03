2cea82b462f6a15e06a22a9c7ee857c7c1d6953d
Roman Zherebko <romanzherebko@Mac-mini-Roman.netis>
2022-06-03 16:40:54 +0300

    init

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Roman Zherebko on 03.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
